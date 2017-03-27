44de04c26d39634c9d5b87fdd2a5810c98302740
nik0405 <nik.dba09@gmail.com>
2017-03-27 14:58:00 +0530

    Update cc.go

--- cc.go
+++ cc.go
@@ -255,7 +255,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		return nil, errors.New("Invalid query function name. Expecting \"query\"")
 	}
 	var custName string // Entities
-	//var custAddressKey string  //Customer address key to read write in ledger as key value of address
+	var custAddressKey string  //Customer address key to read write in ledger as key value of address
 	
 
 	//var err error
